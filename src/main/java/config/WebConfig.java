d714f9016eb8808fcf16545d7363b6d9978fda65
Marianna <34186390+MariWq@users.noreply.github.com>
2023-11-23 12:18:24 +0400

    Update WebConfig.java
    Определяет название файла с настройками

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 import static org.aeonbits.owner.ConfigFactory.*;
 
+@Config.Sources({"classpath:${env}.properties"})
 public interface WebConfig extends Config {
 
     WebConfig BASE_CONFIG = create(WebConfig.class, System.getenv(), System.getProperties());
